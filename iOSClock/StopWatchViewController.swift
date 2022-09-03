788c31324fd16f51abf40671455b9bf5e538a3b4
taek0622 <min4644@gmail.com>
2022-09-03 14:03:42 +0900

    Feat:sparkles:: #2 Do Init Project

---
+++ iOSClock/StopWatchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StopWatchViewController.swift
+//  iOSClock
+//
+//  Created by 김민택 on 2022/09/03.
+//
+
+import UIKit
+
+class StopWatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOSClock/WorldTimeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WorldTimeViewController.swift
+//  iOSClock
+//
+//  Created by 김민택 on 2022/09/03.
+//
+
+import UIKit
+
+class WorldTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
